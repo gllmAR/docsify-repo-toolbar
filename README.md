243a96c60964de41e464d159b345a863b4a8e82a
Gllmar <gllmAR@users.noreply.github.com>
2024-08-17 16:46:02 -0400

    still wip

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # docsify-repo-toolbar
 docsify-repo-toolbar
+
+
+
+Lorem Ipsum
+"Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."
+"There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain..."
+
+
+## section 2
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas dolor est, tempor at quam non, auctor lobortis enim. Nunc egestas facilisis gravida. Nulla at nibh eros. Donec vel nisi scelerisque, efficitur est id, dignissim ipsum. Morbi scelerisque congue ultrices. Praesent at urna ut purus dapibus accumsan. Mauris nec sem arcu. Donec sapien tellus, blandit nec urna non, sagittis molestie lacus. Praesent sodales posuere velit at viverra. Nullam vel consequat erat, in convallis orci. Suspendisse leo eros, fringilla a mauris non, ultrices volutpat neque. Interdum et malesuada fames ac ante ipsum primis in faucibus. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+
+## section 3
+Morbi nec vestibulum massa. Donec eu felis scelerisque, dapibus nunc et, ullamcorper turpis. Nullam eu elementum lectus. Vestibulum suscipit posuere odio, sit amet viverra purus. Fusce ac tortor nisi. Pellentesque fermentum ligula vitae ex sagittis scelerisque. Pellentesque ultricies ex a est malesuada vehicula. Morbi nec tincidunt turpis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque at risus risus. Quisque cursus sem quis faucibus volutpat. Morbi vulputate tempus magna id gravida. Nulla vitae aliquam massa. Maecenas id velit convallis, ultrices dolor venenatis, varius nisi.
+
+## section 4
+
+Donec consectetur erat tellus, quis ultrices velit malesuada id. Nullam erat purus, faucibus eget arcu vel, accumsan sagittis dolor. Vestibulum euismod odio vel lectus egestas, nec commodo diam semper. Mauris scelerisque auctor ante non mattis. Duis semper neque turpis, lacinia pellentesque neque fringilla sed. Vestibulum purus dui, rhoncus et risus et, fringilla finibus velit. Nunc aliquet dui ut nulla ornare, sit amet hendrerit eros laoreet. Etiam bibendum vitae purus sit amet mattis. Nunc sed ante at ligula tristique dapibus eu sagittis neque. Cras leo tellus, congue non orci at, bibendum laoreet ante. Praesent non rutrum felis. Duis enim nisl, tempor non posuere ac, laoreet a velit. Maecenas dapibus risus quam, et elementum lorem laoreet vitae. Donec rhoncus ligula ut ante posuere, sit amet hendrerit sem bibendum. Pellentesque bibendum mi quis aliquam sollicitudin. Mauris semper id urna non ullamcorper.
+
+## section 5
+Etiam varius mauris eget dui bibendum sodales. Donec congue lorem laoreet, tristique dolor nec, elementum lectus. Nunc id metus eget elit eleifend tempus tincidunt in mauris. Vivamus pulvinar ac neque eget convallis. Ut vulputate erat vitae venenatis molestie. Donec mollis vitae massa semper scelerisque. Mauris non augue ut turpis venenatis rutrum et in nisl. Quisque eget sollicitudin nisi. Nulla ipsum nisi, viverra in dignissim sed, convallis in nibh.
+
+Pellentesque pellentesque porttitor nisi, vel rutrum massa finibus eu. Nam congue sem quis velit porttitor, vitae luctus ante pulvinar. Donec fringilla libero nisl, ac venenatis purus egestas ut. Mauris diam nisi, eleifend vel massa gravida, tristique mollis leo. Aenean nec ex non dolor fringilla pulvinar. Fusce dolor enim, pretium quis nulla scelerisque, lobortis viverra diam. Nulla vel rhoncus nulla. Nam posuere metus sed sem malesuada suscipit. Cras vehicula pharetra eros, pellentesque vehicula ex rutrum non. Proin orci dui, tempus vitae dolor sed, convallis vestibulum ante. Ut facilisis ipsum a lorem lobortis, a lacinia tellus ultrices. 
---
+++ docsify-repo-toolbar.js
@@ -0,0 +1,112 @@
+(function() {
+    // Default CSS styles for the repository toolbar button
+    const defaultStyles = `
+        .repo-toolbar-button {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            padding: 10px;
+            color: var(--theme-highlight, #ffcc00);
+            text-decoration: none;
+            position: absolute;
+            top: 0px;
+            left: 0px;
+            transition: opacity 0.3s ease;
+        }
+
+        .repo-toolbar-button svg {
+            width: 24px;
+            height: 24px;
+        }
+
+        body.close .repo-toolbar-button {
+            display: none;
+        }
+    `;
+
+    // Function to add CSS to the document
+    function addStyles(css) {
+        const style = document.createElement('style');
+        style.type = 'text/css';
+        style.appendChild(document.createTextNode(css));
+        document.head.appendChild(style);
+    }
+
+    // Function to get the repository icon based on the repo URL
+    function getRepoIcon(repoUrl) {
+        if (repoUrl.includes('github.com')) {
+            return `
+                <svg width="24" height="24" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82a7.7 7.7 0 012.01-.27c.68 0 1.36.09 2.01.27 1.53-1.03 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"/>
+                </svg>
+            `;
+        } else if (repoUrl.includes('gitlab.com')) {
+            return `
+                <svg width="24" height="24" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M15.97 9.57L14 3.16a.82.82 0 00-1.55-.1l-1.52 4.85H5.07L3.55 3.05a.82.82 0 00-1.55.1l-2 6.4a.92.92 0 00.34 1.03l7.18 5.6c.28.22.67.22.95 0l7.18-5.6c.29-.23.41-.61.34-1.03z"/>
+                </svg>
+            `;
+        } else {
+            return `
+                <svg width="24" height="24" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M3.9 12c0-.8.6-1.4 1.4-1.4h8.5c.8 0 1.4.6 1.4 1.4s-.6 1.4-1.4 1.4H5.3c-.8 0-1.4-.6-1.4-1.4zM5.3 9.7h8.5c.9 0 1.7-.7 1.7-1.7 0-.9-.7-1.7-1.7-1.7H5.3C4.4 6.3 3.7 7 3.7 7.9c0 .9.7 1.7 1.7 1.7zm14 6.7H10c-.9 0-1.7.7-1.7 1.7 0 .9.7 1.7 1.7 1.7h9.3c.8 0 1.4-.6 1.4-1.4s-.6-1.4-1.4-1.4zm0-10.7h-8.5c-.8 0-1.4.6-1.4 1.4s.6 1.4 1.4 1.4h8.5c.9 0 1.7-.7 1.7-1.7 0-.9-.7-1.7-1.7-1.7zM18 12c0 .8-.6 1.4-1.4 1.4H7.2c-.9 0-1.7-.7-1.7-1.7 0-.9.7-1.7 1.7-1.7h9.3c.8 0 1.4.6 1.4 1.4z"/>
+                </svg>
+            `;
+        }
+    }
+
+    // Docsify plugin function
+    function initRepoButton(hook, vm) {
+        hook.mounted(function() {
+            console.log("Docsify plugin mounted");
+
+            // Insert default styles
+            if (!document.querySelector('#repo-toolbar-styles')) {
+                console.log("Adding default styles");
+                addStyles(defaultStyles);
+            }
+
+            // Remove the default GitHub corner link
+            const githubCorner = document.querySelector('.github-corner');
+            if (githubCorner) {
+                console.log("Removing default GitHub corner");
+                githubCorner.remove();
+            }
+
+            // Determine the repo URL and icon
+            const repoUrl = vm.config.repo || '';
+            console.log("Repository URL: ", repoUrl);
+
+            const repoIcon = getRepoIcon(repoUrl);
+
+            // Create the repository button
+            const repoButton = document.createElement('a');
+            repoButton.href = repoUrl;
+            repoButton.target = '_blank';
+            repoButton.className = 'repo-toolbar-button';
+            repoButton.innerHTML = repoIcon;
+
+            // Append the repo button to the sidebar
+            const sidebar = document.querySelector('.sidebar');
+            if (sidebar) {
+                sidebar.appendChild(repoButton);
+            }
+
+            // Toggle visibility based on sidebar state
+            const toggleRepoButtonVisibility = function() {
+                if (document.body.classList.contains('close')) {
+                    repoButton.style.display = 'none';
+                } else {
+                    repoButton.style.display = 'flex';
+                }
+            };
+
+            // Run the function initially and on sidebar toggle
+            toggleRepoButtonVisibility();
+            document.querySelector('.sidebar-toggle').addEventListener('click', toggleRepoButtonVisibility);
+        });
+    }
+
+    // Register the plugin with Docsify
+    window.$docsify.plugins = [].concat(initRepoButton, window.$docsify.plugins);
+})();
